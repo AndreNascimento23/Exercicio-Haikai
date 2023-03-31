3e04a9a2787ea28d54bb295195307b094c4a75f6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-03-31 19:26:32 -0300

    HaiKai Completo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\nNa cidade, a lua:");
+Console.WriteLine("a joia branca que boia");
+Console.WriteLine("na lama da rua.");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\n(Guilherme de Almeida)\n");
+
+Console.ResetColor();
